5fde54a00a3e73f2d1313903e7e103c32cadc176
Mark Perryman <mark.perryman@metaswitch.com>
2016-01-06 15:44:24 +0000

    Improved setup.sh and included git config

--- setup.sh
+++ setup.sh
@@ -2,25 +2,23 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-for filename in `ls $DIR`
+for filename in `ls -a $DIR`
 do
     if [ -f $DIR/$filename -a $filename != "setup.sh" ]
     then
-        if [ -e ~/$filename ! -h ~/$filename ]
+        if [ -e ~/$filename -a ! -h ~/$filename ]
         then
-            mv ~/$filename ~/$filename~
-            echo "Backed up existing ~/$filename"
+            mv ~/$filename ~/$filename~ && echo "Backed up existing ~/$filename"
         fi
-        ln -s $DIR/$filename ~/$filename
-        echo "Linked ~/$filename to $DIR/$filename"
+        ln -s $DIR/$filename ~/$filename && echo "Linked ~/$filename to $DIR/$filename"
     fi
 
 done
 
-mkdir .vim
+mkdir $DIR/.vim
 mv ~/.vim/ftplugin ~/.vim/ftplugin~
 ln -s $DIR/.vim/ftplugin ~/.vim/
 
-mkdir .ssh
+mkdir $DIR/.ssh
 mv ~/.ssh/config ~/.ssh/config~
 ln -s $DIR/Config/.ssh/config ~/.ssh/
